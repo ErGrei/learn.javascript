7379b0f74394ebf4168e1126641f364e0955df3f
Ivan Piskunov <i.s.piskunov@gmail.com>
2024-03-01 14:54:37 +0300

    feat: added analysis of chapters 2.7-2.9

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.clear();
 // 'use strict';
 //  a =1;
 //  console.log(a); reference error
@@ -49,18 +50,16 @@ let currentUserName = "Джон";
 // const BIRTHDAY = '18.04.1982';
 // const age = someCode(birthday);
 
-
 //Типы данных
 
 let num = 10; // number
-let str = 'строка'; // string
+let str = "строка"; // string
 let bool = true; // boolean
 let undef = undefined; // undefined
 let nul = null; // null
 let simbol = Symbol(); // symbol
 let bigNum = 10n; // bigint
-let obj = {} || [] || function(){}; // object
-
+let obj = {} || [] || function () {}; // object
 
 console.log(typeof num); //number
 console.log(typeof str); //string
@@ -70,7 +69,108 @@ console.log(typeof nul); //object 2 варианта!!
 console.log(typeof simbol); //symbol
 console.log(typeof bigNum); //bigint
 console.log(typeof obj); //object
-let foo = function(){};
+let foo = function () {};
 console.log(typeof foo); //function
 console.log(typeof NaN); //number
-1 / 0
+1 / 0;
+
+// Преобразование типов
+//строковое преобразование
+
+const а = 1;
+const b = a.toString();
+console.log(typeof b); //string
+const c = 1;
+const d = String(c);
+console.log(typeof d); //string
+
+//Числовое преобразование
+
+const e = "1";
+const t = Number(e);
+console.log(typeof t); //number
+
+const f = "1";
+const g = parseInt(f);
+console.log(g); //255 при 16//495 при 32
+console.log(typeof g); //number
+
+const h = "1";
+const j = +h;
+console.log(typeof j); //number
+
+const k = "1";
+const l = a * 1;
+console.log(typeof l); //number
+
+const q = "1";
+const w = q / 1;
+console.log(typeof w); //number
+
+const z = null;
+const x = +z; // 0 при null
+console.log(typeof x);
+
+const v = undefined;
+const m = +v; // NaN при undefined
+console.log(typeof m);
+
+const aa = "";
+const bb = +aa; // 0 при пустой строке
+console.log(typeof bb);
+
+const cc = true;
+const dd = +cc; // 1 при true
+console.log(typeof dd);
+
+const ee = false;
+const ff = +ee; // 0 при false
+console.log(typeof ff);
+
+// Булевое преобразование
+
+const qq = "1";
+const ww = Boolean(qq);
+console.log(ww); //true
+
+const rr = "0";
+const tt = Boolean(ee);
+console.log(tt); //false
+
+const yy = 0;
+const uu = Boolean(yy);
+console.log(uu); //false
+
+const oo = 1;
+const pp = Boolean(oo);
+console.log(pp); //true
+
+const mm = null;
+const nn = Boolean(mm);
+console.log(nn); //false
+
+const jj = undefined;
+const kk = Boolean(jj);
+console.log(kk); //false
+
+const ll = "";
+const ss = Boolean(ll);
+console.log(ss); //false
+
+const hh = NaN;
+const gg = Boolean(hh);
+console.log(gg); //false
+
+console.log(typeof -"2"); //number
+
+//Математические операторы
+// +
+// -
+// *
+// /
+// % остаток от деления
+// ** возведение в степень
+// var++ постфиксная форма
+// ++var префиксная форма
+//++ декремент
+//-- инкремент
