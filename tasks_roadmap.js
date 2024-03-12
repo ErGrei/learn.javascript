82aad741913772f2495ee81bb8a650117dc1b365
Ivan Piskunov <i.s.piskunov@gmail.com>
2024-03-12 15:40:55 +0300

    feat: added option for solving the roadmap problem

--- tasks_roadmap.js
+++ tasks_roadmap.js
@@ -1,26 +1,49 @@
-// Реализуйте функцию getNumbersByParity, которая принимает массив чисел в 
+// Реализуйте функцию getNumbersByParity, которая принимает массив чисел в
 // качестве первого аргумента и строку "even" или "odd" в качестве второго.
 //  Функция должна вернуть новый массив, состоящий из четных чисел, если
-//   вторым аргументом было передано "even" и нечетных, если было передано 
+//   вторым аргументом было передано "even" и нечетных, если было передано
 //   "odd". Оба аргумента функции обязательны. Первый обязательно будет массивом,
 //    а второй - строкой "even"/"odd".
-function getNumbersByParity (data, par) {
-    let arr = [...data]
-    if (par === "even") {
-        return arr.filter(num => num % 2 === 0)
-    } else {
-        return arr.filter(num => num % 2 !== 0)
+function getNumbersByParity(data, par) {
+  //   вариант 1
+  //let arr = [...data];
+  //   if (par === "even") {
+  //     return arr.filter((num) => num % 2 === 0);
+  //   } else {
+  //     return arr.filter((num) => num % 2 !== 0);
+  //   }
+
+  //   вариант 2
+  if (par === "even") {
+    isEven(data);
+  } else {
+    isOdd(data);
+  }
+  function isEven(arr) {
+    let arrEven = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        arrEven.push(arr[i]);
+      }
     }
+  }
+  function isOdd(arr) {
+    let arrOdd = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 !== 0) {
+        arrOdd.push(arr[i]);
+      }
+    }
+  }
 }
 
-// Реализуйте функцию hasArrays, которая принимает массив данных разных типов и 
+// Реализуйте функцию hasArrays, которая принимает массив данных разных типов и
 // возвращает true, если этот массив содержит массив внутри себя. Если нет, функция должна вернуть false.
-function hasArrays (arr) {
-if (arr.lehght === 0) {
-    return false
-}
-for (let i = 0; i < arr.length; i++) {
-    Array.isArray(arr[i]) ? true : false
-}
-
+function hasArrays(arr) {
+  if (arr.lehght === 0) {
+    return false;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    Array.isArray(arr[i]) ? true : false;
+  }
 }
