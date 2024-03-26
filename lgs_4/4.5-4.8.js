7f67edd56162e8d9b7d4b4f13477d65e7002c619
Ivan Piskunov <i.s.piskunov@gmail.com>
2024-03-26 14:30:25 +0300

    feat: add JSDoc

--- lgs_4/4.5-4.8.js
+++ lgs_4/4.5-4.8.js
@@ -7,6 +7,17 @@ const obj = {
   friends: ["Pete", "Sam"],
 };
 
+/**
+ * Converts an object into an iterator.
+ *
+ * @param {Object} obj - The object to be converted into an iterator.
+ * @return {Object} - The iterator object with the following properties:
+ *   - The properties of the input object.
+ *   - A `[Symbol.iterator]` method that returns an iterator object with a `next` method.
+ *   - The `next` method returns an object with the following properties:
+ *     - `value`: The value of the current property in the iteration.
+ *     - `done`: A boolean indicating if the iteration is complete.
+ */
 function toIterator(obj) {
   return {
     ...obj,
