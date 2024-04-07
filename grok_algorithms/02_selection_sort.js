b09f0738bdea74afe49fee875453e4277d7fa043
Ivan Piskunov <i.s.piskunov@gmail.com>
2024-04-07 13:12:39 +0300

    init: 02_selection_sort

---
+++ grok_algorithms/02_selection_sort.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+
+
+
+// arr = (length, max) => {
+//   return [...new Array(length)].map(() => Math.round(Math.random() * max));
+// };
+
+// console.log(arr(10,100));
+
