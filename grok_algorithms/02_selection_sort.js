72a76c29498a23b2dcc20a9530652494eb18fdd6
Ivan Piskunov <i.s.piskunov@gmail.com>
2024-04-13 07:37:09 +0300

    feat: add form tasks

--- grok_algorithms/02_selection_sort.js
+++ grok_algorithms/02_selection_sort.js
@@ -4,10 +4,10 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 //O(1) = const
 //O(logn) = 3 (3 ** 2 = 9)
 
-// arr.splice(5,0,100);
-// arr.splice(5,1)
+arr.splice(5,0,100);
+arr.splice(5,1)
 
-// console.log(arr);
+console.log(arr);
 
 arr = (length, max) => {
   return [...new Array(length)].map(() => Math.round(Math.random() * max));
@@ -34,15 +34,15 @@ function selectionSort(arr) {
 let arr2 = [5, 4, 5, 4, 3, 3, 2, 2, 9, 9, 10, 1, 0];
 console.log(selectionSort(arr2));
 
-// function bubbleSort(arr) {
-//   for (let i = 0; i < arr.length - 1; i++) {
-//     for (let j = 0; j < arr.length - 1; j++) {
-//       if (arr[j] > arr[j + 1]) {
-//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-//       }
-//     }
-//   }
-//   return arr;
-// }
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
 
-// console.log(bubbleSort(arr(10,50)));
+console.log(bubbleSort(arr(10,50)));
