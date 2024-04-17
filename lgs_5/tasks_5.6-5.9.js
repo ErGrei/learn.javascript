5b7acb056781e1674a64d0b569f0a607ecddfc6c
Ivan Piskunov <i.s.piskunov@gmail.com>
2024-04-17 14:11:04 +0300

    feat: add lgs tasks

---
+++ lgs_5/tasks_5.6-5.9.js
@@ -0,0 +1,169 @@
+// Создайте функцию unique(arr), которая вернёт массив уникальных, не повторяющихся значений массива arr.
+
+function unique(arr) {
+  return [...new Set(arr)];
+}
+
+let values = [
+  "Hare",
+  "Krishna",
+  "Hare",
+  "Krishna",
+  "Krishna",
+  "Krishna",
+  "Hare",
+  "Hare",
+  ":-O",
+];
+
+console.log(unique(values)); // Hare,Krishna,:-O
+
+// Мы хотели бы получить массив ключей map.keys() в переменную и далее работать с ними, например, применить метод .push.
+
+// Но это не выходит:
+// let map = new Map();
+// map.set("name", "John");
+// let keys = map.keys();
+
+// Error: keys.push is not a function
+// Ошибка: keys.push -- это не функция
+// keys.push("more");
+// Почему? Что нужно поправить в коде, чтобы вызов keys.push сработал?
+
+let map = new Map();
+
+map.set("name", "John");
+//вариант 1
+let keys = Array.from(map.keys());
+
+keys.push("more");
+
+console.log(keys); // name,
+
+// вариант 2
+
+keys = [...map.keys(), "more"];
+
+console.log(keys); // name,  more
+
+// Напишите функцию aclean(arr), которая возвращает массив слов, очищенный от анаграмм.
+
+// Например:
+
+// let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+// alert( aclean(arr) ); // "nap,teachers,ear" или "PAN,cheaters,era"
+
+// Есть объект salaries с произвольным количеством свойств, содержащих заработные платы.
+
+// Напишите функцию sumSalaries(salaries), которая возвращает сумму всех зарплат с помощью метода Object.values и цикла for..of.
+
+// Если объект salaries пуст, то результат должен быть 0.
+
+// Например:
+
+// let salaries = {
+//   "John": 100,
+//   "Pete": 300,
+//   "Mary": 250
+// };
+
+// alert( sumSalaries(salaries) ); // 650
+
+function sumSalaries(salaries) {
+  let sum = 0;
+  for (let value of Object.values(salaries)) {
+    sum += value;
+  }
+
+  return sum; 
+}
+
+let salaries = {
+  John: 100,
+  Pete: 300,
+  Mary: 250,
+};
+
+console.log(sumSalaries(salaries));
+
+
+// Напишите функцию count(obj), которая возвращает количество свойств объекта:
+
+// let user = {
+//   name: 'John',
+//   age: 30
+// };
+
+// alert( count(user) ); // 2
+// Постарайтесь сделать код как можно короче.
+
+function count(obj) {
+  return Object.keys(obj).length;
+}
+
+let user = {
+  name: 'John',
+  age: 30,
+  name2: 'Pete',
+  age2: 30,
+};  
+
+console.log(count(user));
+
+// Есть массив сообщений:
+
+// let messages = [
+//   {text: "Hello", from: "John"},
+//   {text: "How goes?", from: "John"},
+//   {text: "See you soon", from: "Alice"}
+// ];
+// У вас есть к ним доступ, но управление этим массивом происходит где-то ещё. Добавляются новые сообщения и удаляются старые, и вы не знаете в какой момент это может произойти.
+
+// Имея такую вводную информацию, решите, какую структуру данных вы могли бы использовать для ответа на вопрос «было ли сообщение прочитано?». Структура должна быть подходящей, чтобы можно было однозначно сказать, было ли прочитано это сообщение для каждого объекта сообщения.
+
+// P.S. Когда сообщение удаляется из массива messages, оно должно также исчезать из структуры данных.
+
+// P.P.S. Нам не следует модифицировать сами объекты сообщений, добавлять туда свойства. Если сообщения принадлежат какому-то другому коду, то это может привести к плохим последствиям.
+
+
+let messages = [
+    {text: "Hello", from: "John"},
+    {text: "How goes?", from: "John"},
+    {text: "See you soon", from: "Alice"}
+];
+
+let readMessages = new WeakSet();
+
+readMessages.add(messages[0]);
+readMessages.add(messages[1]);
+
+messages.shift();
+
+console.log(readMessages.has(messages[1]));
+
+// Есть массив сообщений такой же, как и в предыдущем задании.
+
+// let messages = [
+//   { text: "Hello", from: "John" },
+//   { text: "How goes?", from: "John" },
+//   { text: "See you soon", from: "Alice" }
+// ];
+// Теперь вопрос стоит так: какую структуру данных вы бы предложили использовать для хранения информации о том, когда сообщение было прочитано?
+
+// В предыдущем задании нам нужно было сохранить только факт прочтения «да или нет». Теперь же нам нужно сохранить дату, и она должна исчезнуть из памяти при удалении «сборщиком мусора» сообщения.
+
+// P.S. Даты в JavaScript можно хранить как объекты встроенного класса Date, которые мы разберём позднее.
+
+ messages = [
+    {text: "Hello", from: "John"},
+    {text: "How goes?", from: "John"},
+    {text: "See you soon", from: "Alice"}
+  ];
+  
+  let readMap = new WeakMap();
+  
+  readMap.set(messages[0], new Date());
+
+  console.log(readMap.get(messages[0]));
+  
