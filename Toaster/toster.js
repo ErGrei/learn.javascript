4cc041002ab993f3b062e1bcc980d93d5b0eb75a
Ivan Piskunov <i.s.piskunov@gmail.com>
2024-04-19 15:46:01 +0300

    feat: add toaster

--- Toaster/toster.js
+++ Toaster/toster.js
@@ -15,49 +15,88 @@
 // код должан правильно работать с точки зрения того что я могу вызвать любой метод в любое время и получить правильный результат. Например я могу увеличить мощность во время рабоыт тостера или установить время в 40 сек, а через 20 сек установить время на 10 сек. Суммарное время работы должно быть 30 сек в этом случае, а не. 40 тк я поменял время а тостер не прекращал работу.
 // Код должен работать так что я могу действовать нерационально нелогично вызывая разные методы но все равно все будет работать каким либо образом и выводить достоверные логи
 
-function Toster() {
+function Toaster() {
+  let power = 5;
+  let timeLeft = 0;
+  let heating = false;
+  let breadInside = false;
+  let heatingTime = 0;
+  let powerConsumed = 0;
 
-  /**
-   * Sets the power of the object.
-   *
-   * @param {number} [param=5] - The power value to set. Defaults to 5.
-   * @return {number|string} - The power value if it is between 1 and 10, otherwise a message indicating the invalid input.
-   */
   this.setPower = function (param = 5) {
     if (param >= 1 && param <= 10) {
-      return param;
+      power = param;
+      return power;
     }
-    return "введите число от 1 до 10";
+    return "Enter a number between 1 and 10 for power.";
   };
 
-  /**
-   * Sets the time left based on the provided parameter.
-   *
-   * @param {number} param - The parameter representing the time left.
-   * @return {number|string} - The time left if it is between 0 and 90, otherwise a message indicating the invalid input.
-   */
   this.setTimeLeft = function (param = 0) {
     if (param >= 0 && param <= 90) {
-      return param;
+      timeLeft = param;
+      return timeLeft;
     }
-    return "введите число от 0 до 90";
+    return "Enter a number between 0 and 90 for time left.";
   };
 
-  this.changeTime = function (param = this.setTimeLeft) {
-    let caunt = param;
-    let decreaseСaunt = () => {
-      return caunt--;
+  this.changeTime = function (param) {
+    let count = param;
+    let intervalId;
+
+    const decreaseCount = () => {
+      count--;
+      heatingTime++;
+      powerConsumed += power;
+      if (count === 0) {
+        clearInterval(intervalId);
+        this.ejectBread();
+      }
     };
+
     if (param > 0) {
-      setInterval(decreaseСaunt, 1000);
+      intervalId = setInterval(decreaseCount, 1000);
+      heating = true;
     } else {
-        clearInterval(decreaseСaunt);
+      clearInterval(intervalId);
+      heating = false;
     }
-    return caunt;
+
+    return count;
+  };
+
+  this.insertBread = function () {
+    if (!breadInside) {
+      breadInside = true;
+      heatingTime = 0;
+      powerConsumed = 0;
+    } else {
+      return "Bread already inside, cannot insert more.";
+    }
+  };
+
+  this.ejectBread = function () {
+    breadInside = false;
+    powerConsumed = 0;
+    heatingTime = 0;
+  };
+
+  this.logStat = function () {
+    return {
+      power,
+      timeLeft,
+      heating,
+      breadInside,
+      heatingTime,
+      powerConsumed,
+    };
+  };
+
+  this.logPower = function () {
+    return `Total power consumed: ${powerConsumed}, Power consumed in last cycle: ${powerConsumed}`;
   };
 }
 
-let toster = new Toster();
-console.log(toster.setPower(4));
-console.log(toster.setTimeLeft(20));
-console.log(toster.changeTime())
+let toaster = new Toaster();
+console.log(toaster.setPower(4));
+console.log(toaster.setTimeLeft(20));
+console.log(toaster.changeTime())
--- lgs_5/5.6-5.9.js
+++ lgs_5/5.6-5.9.js
@@ -1,3 +1,27 @@
+// Задача из таблицы
+// вход
+// customers: массив положительных целых чисел, представляющих очередь. Каждое целое число представляет клиента, а его значение — это количество времени, необходимое ему для оформления заказа.
+// n: положительное целое число, количество касс.
+// выход
+// Функция должна возвращать целое число — общее требуемое время.
+
+function queueTime(customers, n) {
+  if (customers.length === 0) {
+    return 0;
+  }
+  let res = customers.reduce((acc, item) => (acc += item), 0);
+  return Math.floor(res / n);
+}
+
+//   assert.strictEqual(queueTime([], 1), 0);
+//   assert.strictEqual(queueTime([1,2,3,4], 1), 10);
+//   assert.strictEqual(queueTime([2,2,3,3,4,4], 2), 9);
+//   assert.strictEqual(queueTime([1,2,3,4,5], 100), 5); непроходит
+
+//   assert.strictEqual(queueTime([5,3,4],    1), 12);
+//   assert.strictEqual(queueTime([10,2,3,3], 2), 10); непроходит
+//   assert.strictEqual(queueTime([2,3,10,2], 2), 12);непроходит
+
 // Мутабельные методы массивов
 
 //push, pop, shift, unshift, splice, sort, reverse
@@ -72,26 +96,55 @@ for (let entry of Object.entries(obj)) {
   console.log(entry);
 }
 
-
 /**
  * @param {number[]} nums
- * @return {number}
+ * @return {Array<number>}
  */
 var removeDuplicates = function (nums) {
-// вариант 1
+  // вариант 1
   // const res = nums.filter((item, index) => nums.indexOf(item) === index);
 
-// вариант 2
+  // вариант 2
   // return [...new Set(nums)];
 
-// вариант 3
- let res = nums.reduce((acc, i) => {
+  // вариант 3
+  let res = nums.reduce((acc, i) => {
     if (!acc.includes(i)) {
       acc.push(i);
     }
     return acc;
-  },[])
+  }, []);
   return res;
 };
-console.log(removeDuplicates([ 1, 2, 3, 3, 2, 1 ]))
+console.log(removeDuplicates([1, 2, 3, 3, 2, 1]));
 
+// Given an array of the weather conditions, your task is to work out the minimum number of umbrellas he needs to start with in order that he never gets wet. He can start with some umbrellas at home and some at work, but the output is a single integer, the minimum total number.
+
+// The input is an array/list of consecutive half-day weather forecasts. So, e.g. the first value is the 1st day's morning weather and the second value is the 1st day's afternoon weather. The options are:
+
+// Without umbrella:
+
+// "clear",
+// "sunny",
+// "cloudy",
+// "overcast",
+// "windy".
+// With umbrella:
+
+// "rainy",
+// "thunderstorms".
+// e.g. for three days, 6 values:
+
+function minUmbrellas(weather) {
+  let count = 0;
+  const uniqueArr = new Set(weather);
+  uniqueArr.forEach((item) => {
+   if (item === "rainy" || item === "thunderstorms") {
+     count += 1;
+   }
+ })
+  return count;
+}
+console.log(minUmbrellas(["cloudy"]), 0);
+console.log(minUmbrellas(["rainy", "rainy", "rainy", "rainy"]), 1);
+console.log(minUmbrellas(["overcast", "rainy", "clear", "thunderstorms"]), 2);
