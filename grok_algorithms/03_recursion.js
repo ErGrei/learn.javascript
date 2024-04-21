b787a659375fd9852ed670c7a54e33906dda1359
Ivan Piskunov <i.s.piskunov@gmail.com>
2024-04-21 14:08:59 +0300

    feat: Faktorial, fibonacci

--- grok_algorithms/03_recursion.js
+++ grok_algorithms/03_recursion.js
@@ -8,21 +8,16 @@
 // Гарантировать, что рекурсивные вызовы функции корректно управляются с задержкой в одну секунду.
 // Обеспечить чистоту вывода: функция должна четко сообщать пользователю о каждой секунде отсчета и о завершении отсчета.
 
-function recursiveCountdown(seconds = 10) {
-  console.log(`Осталось времени: ${seconds} секунд`);
-  return seconds === 0
-    ? console.log("Отсчет завершен!")
-    : setTimeout(recursiveCountdown, 1000, seconds - 1);
+// function recursiveCountdown(seconds = 10, count ) {
+// if (seconds === 0){
+//   console.log('Отсчет завершен!')
+// } else {
+//   console.log(`Осталось секунд: ${seconds}`)
+//   setTimeout(()=>recursiveCountdown(--seconds),1000)
+// }
+// }
 
-  // if (seconds > 0) {
-  //     console.log(`Осталось времени: ${seconds} секунд`);
-  //     setTimeout(() => recursiveCountdown(seconds - 1), 1000);
-  // } else {
-  //     console.log("Отсчет завершен!");
-  // }
-}
-
-// recursiveCountdown(5);
+// recursiveCountdown();
 
 // Ваша задача:
 // Написать функцию calculateAverageSalaries, которая принимает объект company и
@@ -62,24 +57,25 @@ let company = {
     ],
 
     internals: {
-        sites: [
-          {
-            name: "Peter",
-            salary: 2000,
-          },
-          {
-            name: "Alex",
-            salary: 1800,
-          },
-        ],
-    
-        internals: [
-          {
-            name: "Jack",
-            salary: 1300,
-          },
-        ],
-      },
+      sites: [
+        {
+          name: "Peter",
+          salary: 2000,
+        },
+
+        {
+          name: "Alex",
+          salary: 1800,
+        },
+      ],
+
+      internals: [
+        {
+          name: "Jack",
+          salary: 1300,
+        },
+      ],
+    },
   },
 };
 
@@ -98,36 +94,79 @@ function calculateAverageSalaries(obj, result = {}) {
   return result;
 }
 
-console.log(calculateAverageSalaries(company));
+// console.log(calculateAverageSalaries(company));
 
-const data = calculateAverageSalaries(company);
+// const data = calculateAverageSalaries(company);
 
-function formattedSalary(data, level = 0) {
-  const averageSalary = {};
+// function formattedSalary(data, level = 0) {
+//   const averageSalary = {};
 
-  const getNestedSalaries = (obj, currentLevel) =>
-    Object.values(obj).reduce((acc, value) => {
-      if (currentLevel < level || typeof value === "number") {
-        return acc + value;
-      } else if (typeof value === "object") {
-        return acc + getNestedSalaries(value, currentLevel + 1);
-      }
-      return acc;
-    }, 0);
+//   const getNestedSalaries = (obj, currentLevel) =>
+//     Object.values(obj).reduce((acc, value) => {
+//       if (currentLevel < level || typeof value === "number") {
+//         return acc + value;
+//       } else if (typeof value === "object") {
+//         return acc + getNestedSalaries(value, currentLevel + 1);
+//       }
+//       return acc;
+//     }, 0);
 
-  for (const [key, value] of Object.entries(data)) {
-    if (typeof value === "number") {
-      averageSalary[key] = value;
-    } else if (typeof value === "object") {
-      const nestedSalaries = getNestedSalaries(value, level + 1);
-      averageSalary[key] = nestedSalaries / Object.values(value).length;
-    }
-  }
+//   for (const [key, value] of Object.entries(data)) {
+//     if (typeof value === "number") {
+//       averageSalary[key] = value;
+//     } else if (typeof value === "object") {
+//       const nestedSalaries = getNestedSalaries(value, level + 1);
+//       averageSalary[key] = nestedSalaries / Object.values(value).length;
+//     }
+//   }
 
-  return averageSalary;
+//   return averageSalary;
+// }
+
+// console.log(formattedSalary(calculateAverageSalaries(company)));
+
+// function factorial(num, kesh = { 0: 1 }) {
+  //Хвостовая рекурсия
+
+
+
+ //Кеширование
+
+  // if (kesh.hasOwnProperty(num)) {
+  //   return kesh[num];
+  // }
+
+  // kesh[num] = num * factorial(num -1, kesh);
+  // return kesh[num];
+
+// рекурсия
+
+  // if (num === 1) {
+  //   return 1;
+  // } else {
+  //   return num * factorial(--num);
+  // }
+// }
+
+// console.log(factorial(3));
+
+
+function fibonacciTail(n, a = 0, b = 1) {
+  if (n === 0) return a;
+  if (n === 1) return b;
+  return fibonacciTail(n - 1, b, a + b);
 }
 
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
+function fibonacciMemo(n, memo = {}) {
+  if (memo[n] !== undefined) return memo[n];
+  if (n <= 1) return n;
+  memo[n] = fibonacciMemo(n - 1, memo) + fibonacciMemo(n - 2, memo);
+  return memo[n];
+}
 
-
-console.log(formattedSalary(calculateAverageSalaries(company)));
+console.log(fibonacciMemo(10000));
