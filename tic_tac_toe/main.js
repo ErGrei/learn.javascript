a1208497f266eccd72ffce4f7e39ccf8363fde0d
Ivan Piskunov <i.s.piskunov@gmail.com>
2024-04-28 05:42:47 +0300

    feat: added winning conditions

--- tic_tac_toe/main.js
+++ tic_tac_toe/main.js
@@ -1,6 +1,7 @@
 newGame();
 let xIsNext = true;
-let step = [];
+let stepX = [];
+let stepO = [];
 function newGame() {
   const bodi = document.querySelector(".body");
   const wraper = document.createElement("div");
@@ -24,8 +25,9 @@ function createElements(wraper) {
 function clickCell(e) {
   const cell = e.target;
   const index = cell.getAttribute("data-n");
+  checkWin(index, cell);
   gameMoves(cell);
-  checkWin(index);
+  checkDraw()
 }
 
 function eventListener() {
@@ -33,9 +35,11 @@ function eventListener() {
   cells.forEach((cell) => {
     cell.addEventListener("click", clickCell);
   });
+  
 }
 
 function gameMoves(cell) {
+  
   // console.log(xIsNext);
   toggle();
   function toggle() {
@@ -43,34 +47,95 @@ function gameMoves(cell) {
       return;
     }
     if (xIsNext) {
-      cell.textContent = "O";
+      cell.textContent = "X";
       xIsNext = false;
     } else {
-      cell.textContent = "X";
+      cell.textContent = "O";
       xIsNext = true;
     }
   }
 }
 
-function checkWin(indexStep) {
+function checkWin(indexStep, cell) {
   const wim = ["012", "345", "678", "036", "147", "258", "048", "246"];
-
+  if (cell.textContent !== "") {
+    return;
+  }
   // console.log(indexStep);
   if (xIsNext) {
-    step.push(indexStep);
-    console.log(step);
+    stepX.push(indexStep);
+    console.log(stepX);
+    checkWinCount(stepX);
+  } else {
+    stepO.push(indexStep);
+    console.log(stepO);
+    checkWinCount(stepO);
   }
 
-  for (let i = 0; i < wim.length; i++) {
-    const winPattern = wim[i];
-    let count = 0;
-    winPattern.split("").forEach((char) => {
-      if (step.includes(char)) {
-        count++;
+  function checkWinCount(ArrStep) {
+    if (ArrStep.length >= 3) {
+      for (let i = 0; i < wim.length; i++) {
+        const winPattern = wim[i];
+        let count = 0;
+        winPattern.split("").forEach((char) => {
+          if (ArrStep.includes(char)) {
+            count++;
+          }
+          if (count === 3) {
+            showVictory(xIsNext);
+          }
+        });
       }
-    });
-    if (count === 3) {
-      console.log('win');
     }
   }
+}
+
+function showVictory(xIsNext) {
+  console.log("Победил " + (xIsNext ? "X" : "O"));
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.removeEventListener("click", clickCell);
+  });
+  const wraper = document.querySelector(".wraper");
+  // wraper.style.opacity = "0.5";
+  const popUp = document.createElement("div");
+  wraper.append(popUp);
+  popUp.classList.add("popUp");
+  const notification = document.createElement("div");
+  notification.classList.add("notification");
+  popUp.append(notification);
+  const victory = document.createElement("p");
+  if (xIsNext === "null") {
+    victory.textContent = "Ничья";
+  } else {
+    victory.textContent = "Победил " + (xIsNext ? "X" : "O");
+  }
+  notification.append(victory);
+  const button = document.createElement("button");
+  notification.append(button);
+  button.classList.add("button");
+  button.textContent = "Новая игра";
+  button.addEventListener("click", restart);
+}
+
+function checkDraw() {
+  const cells = document.querySelectorAll(".cell");
+  let count = 0;
+  cells.forEach((cell) => {
+    if (cell.textContent !== "") {
+      count++;
+    }
+  })
+  if (count === 9) {
+    showVictory("null");
+  }
+}
+
+function restart() {
+  xIsNext = true;
+  stepX = [];
+  stepO = [];
+  const wraper = document.querySelector(".wraper");
+  wraper.remove();
+  newGame();
 }
