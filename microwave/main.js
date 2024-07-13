8ce562839016e0edfcc92def487afb14fe12bd53
ErGrei <i.s.piskunov@gmail.com>
2024-07-13 23:47:08 +0900

    feat: added static properties and created a child class

--- microwave/main.js
+++ microwave/main.js
@@ -1,15 +1,37 @@
 class Microwave {
-  #power = 0;
+  static maximumNumberOfStarts = 1;
+  #maxPower = 90;
   constructor() {
     this.on = false;
     this.time = 0;
     this.interval;
+    this.power = 0;
   }
 
+  static getMaximumNumberOfStarts() {
+    return Microwave.maximumNumberOfStarts;
+  }
+
+  static setMaximumNumberOfStarts(number) {
+    Microwave.maximumNumberOfStarts = number;
+  }
+
+  static resetMaximumNumberOfStarts() {
+    Microwave.maximumNumberOfStarts = 5;
+  }
+
+  setMaxPower(power) {
+    this.#maxPower = power;
+    return this.#maxPower;
+  }
+
+  getMaxPower() {
+    return this.#maxPower;
+  }
   setPower(power) {
-    if (power >= 1 && power <= 10) {
-      this.#power = power;
-      return this.#power;
+    if (power >= 1 && power <= this.#maxPower) {
+      this.power = power;
+      return this.power;
     }
     console.log("Enter a number between 1 and 10 for power");
   }
@@ -22,7 +44,7 @@ class Microwave {
     console.log("Enter a number between 0 and 90 for time left");
   }
   getPower() {
-    return this.#power;
+    return this.power;
   }
 
   getTime() {
@@ -30,13 +52,18 @@ class Microwave {
   }
 
   turnOn() {
-    if (this.on === false) {
+    if (Microwave.maximumNumberOfStarts === 0) {
+      console.error("contact the service center");
+      return;
+    } else if (this.on === false) {
       this.on = true;
       this.timeCountDown(this.time);
+      Microwave.maximumNumberOfStarts--;
     } else {
       console.log("Microwave already on");
     }
   }
+
   turnOff() {
     if (this.on === true) {
       this.on = false;
@@ -56,7 +83,8 @@ class Microwave {
       console.log(`Time left:  ${seconds}  seconds`);
       this.interval = setTimeout(() => {
         this.time = --this.time;
-        this.timeCountDown(--seconds)}, 1000);
+        this.timeCountDown(--seconds);
+      }, 1000);
     }
   }
   pause() {
@@ -78,12 +106,42 @@ class Microwave {
       console.log("Microwave already on");
     }
   }
-
 }
 
-const myMicrowave1 = new Microwave();
+class castomMicrowave extends Microwave {
+  constructor(color, manufacturer) {
+    super();
+    this.color = color;
+    this.manufacturer = manufacturer;
+  }
+  getColor() {
+    return this.color;
+  }
+  getManufacturer() {
+    return this.manufacturer;
+  }
+  bakeBread() {
+    if (this.getMaxPower() < 50) {
+      console.log("this function is not available");
+    } else {
+      this.power = 60;
+      this.time = 5;
+      this.turnOn();
+    }
+  }
+}
+
+const myMicrowave1 = new castomMicrowave("black", "Tefal");
+
 myMicrowave1.setPower(5);
-myMicrowave1.setTime(20);
-myMicrowave1.turnOn();
-setTimeout(() => myMicrowave1.pause(), 5000);
-setTimeout(() => myMicrowave1.resume(), 7000);
+myMicrowave1.setTime(5);
+myMicrowave1.bakeBread();
+setTimeout(() => myMicrowave1.pause(), 2000);
+setTimeout(() => myMicrowave1.resume(), 4000);
+setTimeout(() => {
+  myMicrowave1.setPower(5);
+  myMicrowave1.setTime(5);
+  myMicrowave1.turnOn();
+}, 10000);
+
+setTimeout(() => console.log("qqqq"), 12000);
