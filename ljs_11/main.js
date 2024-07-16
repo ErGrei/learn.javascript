2691e9c72db3881a5d120e0519d6612b408fc7f5
ErGrei <i.s.piskunov@gmail.com>
2024-07-16 23:14:42 +0900

    feat: add async function

--- ljs_11/main.js
+++ ljs_11/main.js
@@ -18,19 +18,46 @@ function A(x) {
 }
 A("1");
 
-
 new Promise((resolve, reject) => {
-  resolve('1');
-}).then(( val) => {
-    return val + 2;
-}).then(( val) => {
-    return val + 3;
-}).then(( val) => {
-    return val + 2;
-}).then(( val) => {
-    return val + 1;
-}).then(( val) => {
-    console.log(val +5); //вернет 123215
-}).catch((err) => {
-    console.log(err);
+  resolve("1");
 })
+  .then((val) => {
+    return val + 2;
+  })
+  .then((val) => {
+    return val + 3;
+  })
+  .then((val) => {
+    return val + 2;
+  })
+  .then((val) => {
+    return val + 1;
+  })
+  .then((val) => {
+    console.log(val + 5); //вернет 123215
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// ******************************************************************************
+
+const block = document.querySelector(".block");
+
+let item = 0;
+
+function add() {
+  do {
+    item++;
+    block.value = item;
+  } while (item < 10);
+
+  if (item < 1000) {
+    // setTimeout(add);
+    // Promise.resolve().then(add);
+  }
+}
+setTimeout(() => {
+  console.log(item);
+})
+add();
