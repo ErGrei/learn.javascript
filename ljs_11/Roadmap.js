b42b699d6353999663f629d05a14387657e3ec4e
ErGrei <i.s.piskunov@gmail.com>
2024-07-16 23:39:08 +0900

    feat:added methods Promise resolve and reject

--- ljs_11/Roadmap.js
+++ ljs_11/Roadmap.js
@@ -1,10 +1,24 @@
 class castomPromise {
-  PANDING = "pending";
-  RESOLVED = "resolved";
-  REJECTED = "rejected";
-  constructor() {
-    this.state = PANDING;
+  #PANDING = "pending";
+  #RESOLVED = "resolved";
+  #REJECTED = "rejected";
+  constructor(executor) {
+    this.state = this.#PANDING;
     this.value = null;
     this.callbacks = [];
   }
+
+  resolve = (value) => {
+    if (this.state === this.#PANDING) {
+      this.state = this.#RESOLVED;
+      this.value = value;
+    }
+  };
+
+  reject = (error) => {
+    if (this.state === this.#PANDING) {
+      this.state = this.#REJECTED;
+      this.value = error;
+    }
+  };
 }
