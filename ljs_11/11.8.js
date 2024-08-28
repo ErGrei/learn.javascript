3869d2f55617ce2c3dbefed755abfd45af70f9b9
ErGrei <i.s.piskunov@gmail.com>
2024-08-28 21:16:38 +0400

    feat: completed the tasks of Chapter 11.8

--- ljs_11/11.8.js
+++ ljs_11/11.8.js
@@ -1,17 +1,26 @@
 // Перепишите один из примеров раздела Цепочка промисов, используя async/await вместо .then/catch:
 
 function loadJson(url) {
-  return fetch(url)
-    .then(response => {
-      if (response.status == 200) {
-        return response.json();
-      } else {
-        throw new Error(response.status);
-      }
-    })
+  return fetch(url).then((response) => {
+    if (response.status == 200) {
+      return response.json();
+    } else {
+      throw new Error(response.status);
+    }
+  });
 }
 
-loadJson('no-such-user.json') // (3)
+async function loadJson(url) {
+  let response = await fetch(url);
+  if (response.status == 200) {
+    let json = await response.json();
+    return json;
+  } else {
+    throw new Error(response.status);
+  }
+}
+
+loadJson("no-such-user.json") // (3)
   .catch(alert); // Error: 404
 
 //   Ниже пример из раздела Цепочка промисов, перепишите его, используя async/await вместо .then/catch.
@@ -21,20 +30,29 @@ loadJson('no-such-user.json') // (3)
 class HttpError extends Error {
   constructor(response) {
     super(`${response.status} for ${response.url}`);
-    this.name = 'HttpError';
+    this.name = "HttpError";
     this.response = response;
   }
 }
 
 function loadJson(url) {
-  return fetch(url)
-    .then(response => {
-      if (response.status == 200) {
-        return response.json();
-      } else {
-        throw new HttpError(response);
-      }
-    })
+  return fetch(url).then((response) => {
+    if (response.status == 200) {
+      return response.json();
+    } else {
+      throw new HttpError(response);
+    }
+  });
+}
+
+async function loadJson(url) {
+  let response = await fetch(url);
+  if (response.status == 200) {
+    let json = await response.json();
+    return json;
+  } else {
+    throw new HttpError(response);
+  }
 }
 
 // Запрашивать логин, пока github не вернёт существующего пользователя.
@@ -42,11 +60,11 @@ function demoGithubUser() {
   let name = prompt("Введите логин?", "iliakan");
 
   return loadJson(`https://api.github.com/users/${name}`)
-    .then(user => {
+    .then((user) => {
       alert(`Полное имя: ${user.name}.`);
       return user;
     })
-    .catch(err => {
+    .catch((err) => {
       if (err instanceof HttpError && err.response.status == 404) {
         alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
         return demoGithubUser();
@@ -56,20 +74,36 @@ function demoGithubUser() {
     });
 }
 
+async function demoGithubUser() {
+  while (true) {
+    let name = prompt("Введите логин?", "iliakan");
+    try {
+      let user = await loadJson(`https://api.github.com/users/${name}`);
+      alert(`Полное имя: ${user.name}.`);
+      break
+    } catch (err) {
+      if (err instanceof HttpError && err.response.status == 404) {
+        alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
+      } else {
+        throw err;
+      }
+    }
+  }
+  alert(`Полное имя: ${user.name}.`);
+  return user;
+}
+
 demoGithubUser();
 
 // Есть «обычная» функция. Как можно внутри неё получить результат выполнения async–функции?
 
 async function wait() {
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   return 10;
 }
 
 function f() {
-  // ...что здесь написать?
-  // чтобы вызвать wait() и дождаться результата "10" от async–функции
-  // не забывайте, здесь нельзя использовать "await"
+  wait().then((result) => console.log(result));
 }
 // P.S. Технически задача очень простая, но этот вопрос часто задают разработчики, недавно познакомившиеся с async/await.
-
